6b528db3e229abeb77e593b9a379f45aa24d48a9
Edgar Chen <echen@mozilla.com>
2015-07-31 18:43:53 +0800

    Bug 1121378 - Fix build error

--- emulator/qtools/Android.mk
+++ emulator/qtools/Android.mk
@@ -14,6 +14,7 @@ LOCAL_SRC_FILES := post_trace.cpp trace_reader.cpp decoder.cpp
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := post_trace
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -22,6 +23,7 @@ LOCAL_SRC_FILES := read_trace.cpp trace_reader.cpp decoder.cpp armdis.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := read_trace
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -30,6 +32,7 @@ LOCAL_SRC_FILES := check_trace.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := check_trace
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -38,6 +41,7 @@ LOCAL_SRC_FILES := bb_dump.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := bb_dump
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -46,6 +50,7 @@ LOCAL_SRC_FILES := bb2sym.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := bb2sym
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -54,6 +59,7 @@ LOCAL_SRC_FILES := profile_trace.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := profile_trace
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -62,6 +68,7 @@ LOCAL_SRC_FILES := bbprof.cpp trace_reader.cpp decoder.cpp armdis.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := bbprof
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -70,6 +77,7 @@ LOCAL_SRC_FILES := q2g.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := q2g
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -78,6 +86,7 @@ LOCAL_SRC_FILES := q2dm.cpp trace_reader.cpp decoder.cpp armdis.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := q2dm
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -86,6 +95,7 @@ LOCAL_SRC_FILES := coverage.cpp trace_reader.cpp decoder.cpp armdis.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := coverage
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -94,6 +104,7 @@ LOCAL_SRC_FILES := stack_dump.cpp trace_reader.cpp decoder.cpp armdis.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := stack_dump
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -102,6 +113,7 @@ LOCAL_SRC_FILES := check_stack.cpp trace_reader.cpp decoder.cpp armdis.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := check_stack
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -109,6 +121,7 @@ LOCAL_SRC_FILES := hist_trace.cpp trace_reader.cpp decoder.cpp
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := hist_trace
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -116,6 +129,7 @@ LOCAL_SRC_FILES := read_addr.cpp trace_reader.cpp decoder.cpp
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := read_addr
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -123,6 +137,7 @@ LOCAL_SRC_FILES := read_pid.cpp trace_reader.cpp decoder.cpp
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := read_pid
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -130,6 +145,7 @@ LOCAL_SRC_FILES := exc_dump.cpp trace_reader.cpp decoder.cpp
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := exc_dump
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -138,6 +154,7 @@ LOCAL_SRC_FILES := read_method.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := read_method
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -146,6 +163,7 @@ LOCAL_SRC_FILES := profile_pid.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := profile_pid
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -154,4 +172,5 @@ LOCAL_SRC_FILES := dump_regions.cpp trace_reader.cpp decoder.cpp \
 LOCAL_C_INCLUDES += $(common_includes)
 LOCAL_CFLAGS += $(common_cflags)
 LOCAL_MODULE := dump_regions
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
